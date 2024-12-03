22989585288122a2c23b53e866dece48964fb6bd
Solanki Gaurav <solankigaurav2107@gmail.com>
2024-12-03 18:48:59 +0530

    react  ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useRef } from 'react';
 import './App.css';
+import Input from './Input';
 
 function App() {
-  
+  const inputRef = useRef(null)
   return (
     <div className="App">
+      <Input ref={inputRef} />
+      <button onClick={() => inputRef.current.focus()}>Focus input</button>
     </div>
   );
 }
