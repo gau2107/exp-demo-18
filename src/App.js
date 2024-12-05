4a9d59159a54f63984f4c3e59401cc77efb8cd63
Solanki Gaurav <solankigaurav2107@gmail.com>
2024-12-05 11:47:35 +0530

    useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useLayoutEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  
+  const boxRef = useRef();
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect"); // calls first
+    boxRef.current.style.backgroundColor = "blue"; // Immediate change before showing
+  });
+
+  useEffect(() => {
+    console.log("useEffect");
+    boxRef.current.style.backgroundColor = "red"; // After showing
+  });
   return (
     <div className="App">
+      return <div ref={boxRef} style={{ width: 100, height: 100, backgroundColor: "green" }} />
     </div>
   );
 }
